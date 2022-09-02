1ac91be65711b2f0aa164686b0e1ee804eca311a
m.kodama <m.kodama@dreamcareer.co.jp>
2022-09-02 15:33:41 +0900

    タロットカードの動作完成

--- Tarot-choice/js/jquery.baraja.js
+++ Tarot-choice/js/jquery.baraja.js
@@ -12,10 +12,6 @@
 	
 	'use strict';
 
-	// global
-	var Modernizr = window.Modernizr;
-
-	jQuery.fn.reverse = [].reverse;
 
 	$.Baraja = function( options, element ) {
 		
@@ -24,20 +20,12 @@
 		
 	};
 
-	// the options
-	$.Baraja.defaults = {
-		// default transition speed
-		speed : 300,
-		// default transition easing
-		easing : 'ease-in-out'
-	};
-
 	$.Baraja.prototype = {
 
 		_init : function( options ) {
 			
 			// options
-			this.options = $.extend( true, {}, $.Baraja.defaults, options );
+			this.options = $.extend( true, {}, options );
 
 			var transEndEventNames = {
 				'WebkitTransition' : 'webkitTransitionEnd',
@@ -48,7 +36,7 @@
 			};
 			this.transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ];
 
-			this._setDefaultFanSettings();
+			//this._setDefaultFanSettings();
 
 			this.$items = this.$el.children( 'li' );
 			this.itemsCount = this.$items.length;
@@ -59,71 +47,12 @@
 			this.supportTransitions = Modernizr.csstransitions;
 			// opened/closed deck
 			this.closed = true;
-			// lowest value for the z-index given to the items
+			// アイテムに与えられた z-index の最小値
 			this.itemZIndexMin = 1000;
-			// sets the item's z-index value
+			// アイテムの z-index 値を設定します
 			this._setStack();
-			// initialize some events
-			this._initEvents();
-
 		},
-		_setDefaultFanSettings : function() {
 
-			this.fanSettings = {
-				// speed for opening/closing
-				speed : 500,
-				// easing for opening/closing
-				easing : 'ease-out',
-				// difference/range of possible angles that the items will have
-				// example: with range:90 and center:false the first item
-				// will have 0deg and the last one 90deg;
-				// if center:true, then the first one will have 45deg
-				// and the last one -45deg; in both cases the difference is 90deg
-				range : 90,
-				// this defines the position of the first item 
-				// (to the right, to the left)
-				// and its angle (clockwise / counterclockwise)
-				direction : 'right',
-				// transform origin:
-				// you can also pass a minX and maxX, meaning the left value 
-				// will vary between minX and maxX 
-				origin : { x : 25, y : 100 },
-				// additional translation of each item
-				translation : 0,
-				// if the cards should be centered after the transform 
-				// is applied
-				center : true,
-				// add a random factor to the final transform
-				scatter : false
-			};
-
-		},
-		_validateDefaultFanSettings : function( settings ) {
-
-			if( !settings.origin ) {
-				settings.origin = this.fanSettings.origin;
-			}
-			else {
-				settings.origin.x = settings.origin.x || this.fanSettings.origin.x;
-				settings.origin.y = settings.origin.y || this.fanSettings.origin.y;
-			}
-			settings.speed = settings.speed || this.fanSettings.speed;
-			settings.easing = settings.easing || this.fanSettings.easing;
-			settings.direction = settings.direction || this.fanSettings.direction;
-			settings.range = settings.range || this.fanSettings.range;
-			settings.translation = settings.translation || this.fanSettings.translation;
-			if( settings.center == undefined ) {
-				settings.center = this.fanSettings.center
-			}
-			if( settings.scatter == undefined ) {
-				settings.scatter = this.fanSettings.scatter
-			}
-			
-			this.direction = settings.direction;
-
-			return settings;
-
-		},
 		_setStack : function( $items ) {
 
 			var self = this;
@@ -133,50 +62,6 @@
 
 				$( this ).css( 'z-index', self.itemZIndexMin + self.itemsCount - 1 - i );
 
-			} );
-
-		},
-		_updateStack : function( $el, dir ) {
-
-			var currZIndex = Number( $el.css( 'z-index' ) ),
-				newZIndex = dir === 'next' ? this.itemZIndexMin - 1 : this.itemZIndexMin + this.itemsCount,
-				extra = dir === 'next' ? '+=1' : '-=1';
-			
-			$el.css( 'z-index', newZIndex );
-
-			this.$items.filter( function() {
-
-				var zIdx = Number( $( this ).css( 'z-index' ) ),
-					cond = dir === 'next' ? zIdx < currZIndex : zIdx > currZIndex
-				
-				return cond;
-
-			} ).css( 'z-index', extra );
-
-		},
-		_initEvents : function() {
-
-			var self = this;
-
-			this.$el.on( 'click.baraja', 'li', function() {
-
-				if( !self.isAnimating ) {
-
-					self._move2front( $( this ) );
-
-				}
-
-			} );
-
-		},
-		_resetTransition : function( $el ) {
-
-			$el.css( {
-				'-webkit-transition' : 'none',
-				'-moz-transition' : 'none',
-				'-ms-transition' : 'none',
-				'-o-transition' : 'none',
-				'transition' : 'none'
 			} );
 
 		},
@@ -254,122 +139,12 @@
 			}
 
 		},
-		_navigate : function( dir ) {
-
-			this.closed = false;
-
-			var self = this, 
-				extra = 15,
-				cond = dir === 'next' ? self.itemZIndexMin + self.itemsCount - 1 : self.itemZIndexMin,
-				$item = this.$items.filter( function() {
-					
-					return Number( $( this ).css( 'z-index' ) ) === cond;
-
-				} ),
-				translation = dir === 'next' ? $item.outerWidth( true ) + extra : $item.outerWidth( true ) * -1 - extra,
-				rotation = dir === 'next' ? 5 : 5 * -1;
-				
-			this._setTransition( $item, 'transform', this.options.speed, this.options.easing );
-
-			this._applyTransition( $item, { transform : 'translate(' + translation + 'px) rotate(' + rotation + 'deg)' }, function() {
-
-				$item.off( self.transEndEventName );
-				self._updateStack( $item, dir );
-
-				self._applyTransition( $item, { transform : 'translate(0px) rotate(0deg)' }, function() {
-					
-					$item.off( self.transEndEventName );
-					self.isAnimating = false;
-					self.closed = true;
-
-				} );
-
-			} );
-
-		},
-		_move2front : function( $item ) {
-
-			this.isAnimating = true;
-
-			var self = this,
-				isTop = Number( $item.css( 'z-index' ) ) === this.itemZIndexMin + this.itemsCount - 1,
-				callback = isTop ? function() { self.isAnimating = false; } : function() { return false; },
-				$item = isTop ? null : $item;
-
-			// if it's the one with higher z-index, just close the baraja
-			if( !this.closed ) {
-
-				this._close( callback, $item );
-
-			}
-			else {
-
-				this._fan();
-
-			}
-
-			if( isTop ) {
-				return false;
-			}
-
-			this._resetTransition( $item );
-			this._setOrigin( $item, 50, 50 );
-
-			$item.css( {
-				opacity : 0,
-				transform : 'scale(2) translate(100px) rotate(20deg)'
-			} );
-
-			this._updateStack( $item, 'prev' );
-
-			setTimeout( function() {
-
-				self._setTransition( $item, 'all', self.options.speed, 'ease-in' );
-				self._applyTransition( $item, { transform : 'none', opacity : 1 }, function() {
-
-					$item.off( self.transEndEventName );
-					self.isAnimating = false;
-
-				} );
-
-			}, this.options.speed / 2 );
-
-		},
-		_close : function( callback, $item ) {
-
-			var self = this,
-				$items = self.$items,
-				force = this.closed ? true : false;
-
-			if( $item ) {
-				$items = $items.not( $item );
-			}
-
-			this._applyTransition( $items, { transform : 'none' }, function() {
-				
-				self.closed = true;
-				$items.off( self.transEndEventName );
-				self._resetTransition( $items );
-				setTimeout(function() {
-					
-					self._setOrigin( $items, 50, 50 );
-					
-					if( callback ) {
-						callback.call();
-					}
-
-				}, 25);
-
-			}, force );
-
-		},
+		
 		_fan : function( settings ) {
 
 			var self = this;
 
 			this.closed = false;
-
-			settings = this._validateDefaultFanSettings( settings || {} );
 			
 			// set transform origins
 			// if minX and maxX are passed:
@@ -454,48 +229,6 @@
 			} );
 
 		},
-		// adds new elements to the deck
-		_add : function( $elems ) {
-
-			var self = this, 
-				newElemsCount = $elems.length, cnt = 0;
-
-			$elems.css( 'opacity', 0 ).appendTo( this.$el );
-
-			// reset
-			this.$items = this.$el.children( 'li' );
-			this.itemsCount = this.$items.length;
-			
-			// set z-indexes
-			this._setStack( $elems );
-			
-			// animate new items
-			$elems.css( 'transform', 'scale(1.8) translate(200px) rotate(15deg)' ).reverse().each( function( i ) {
-
-				var $el = $( this );
-
-				self._setTransition( $el, 'all', 500, 'ease-out', i * 200 );
-				self._applyTransition( $el, { transform : 'none', opacity : 1 }, function() {
-
-					++cnt;
-					
-					$el.off( self.transEndEventName );
-					self._resetTransition( $el );
-
-					if( cnt === newElemsCount ) {
-						self.isAnimating = false;
-					}
-
-				} );
-
-			} );
-
-		},
-		_allowAction : function() {
-
-			return this.itemsCount > 1;
-
-		},
 		_prepare : function( callback ) {
 
 			var self = this;
@@ -516,13 +249,12 @@
 			}
 
 		},
+
 		_dispatch : function( action, args ) {
 
 			var self = this;
 
-			if( ( ( action === this._fan || action === this._navigate ) && !this._allowAction() ) || this.isAnimating ) {
-				return false;
-			}
+			action === this._fan
 
 			this.isAnimating = true;
 			
@@ -533,42 +265,16 @@
 			} );
 
 		},
-		// public method: closes the deck
-		close : function( settings ) {
 
-			if( this.isAnimating ) {
-				return false;
-			}
-			this._close();
-
-		},
-		// public method: shows next item
-		next : function() {
-
-			this._dispatch( this._navigate, 'next' );
-
-		},
-		// public method: shows previous item
-		previous : function() {
-
-			this._dispatch( this._navigate, 'prev' );
-
-		},
-		// public method: opens the deck
+		// public メソッド: デッキを開きます
 		fan : function( settings ) {
 
 			this._dispatch( this._fan, settings );
 
 		},
-		// public method: adds new elements
-		add : function ( $elems ) {
-
-			this._dispatch( this._add, $elems );
-
-		}
 
 	};
-	
+
 	var logError = function( message ) {
 
 		if ( window.console ) {
@@ -609,6 +315,7 @@
 			});
 		
 		} 
+		 
 		else {
 		
 			this.each(function() {
@@ -627,6 +334,7 @@
 			});
 		
 		}
+		
 		
 		return instance;
 		
