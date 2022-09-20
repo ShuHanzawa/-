668d85541f42a4257dfb05c06598d3ae5e350cfb
w-hanabusa <w.hanabusa@g.dreamcareer.co.jp>
2022-09-20 15:59:22 +0900

    相対パスまだです

--- fortuming/public/javascripts/horoResult.js
+++ fortuming/public/javascripts/horoResult.js
@@ -1,25 +1,17 @@
-$(async function ()  {
+window.onload = function() {
+    // URLを取得
+    const url = new URL(window.location.href);
+    // URLSearchParamsオブジェクトを取得
+    const params = url.searchParams;
+    // consoleに受け取ったパラメータを出力
+    console.log(params);
+    // パラメータから「username」を取得
+    const horoscope = params.get("horoscope");
+    const blood = params.get("blood");
+    const eto = params.get("eto");
+    console.log(horoscope + blood + eto);
     
-    window.onload = function() {
-        // URLを取得
-        const url = new URL(window.location.href);
-        // URLSearchParamsオブジェクトを取得
-        const params = url.searchParams;
-        // consoleに受け取ったパラメータを出力
-        console.log(params);
-        // パラメータから「username」を取得
-        const username = params.get("username");
-        console.log(username);          
-    }
-    
-    
-    const requestData = {
-    star: $("#deta radio[name=horoscope]").val(),
-    blood: $("#deta radio[name=blood]").val(),
-    eto: $("#deta radio[name=blood]").val()
-  };
-
-  // 日付を xxxx-xx-xx　に変換
+     // 日付を xxxx-xx-xx　に変換
 const date = new Date();
 const month = Number(("0" + (date.getMonth() +1)).slice(-2));
 const day = Number(("0" + (date.getDate() )).slice(-2));
@@ -31,19 +23,19 @@ const yydd = (month + day);
   const color = Math.floor( Math.random() * colors.length );
   const item = Math.floor( Math.random() * items.length );
 
-  const result = (Number(requestData.star) + Number(requestData.blood) + Number(requestData.eto) + yydd) % 5;
+  const result = (Number(horoscope) + Number(blood) + Number(eto) + yydd) % 5;
   console.log(result);
 
 switch (result){
-    case 0: $("#result").text("絶好調！");
+    case 0: $("#result").text("☆☆☆☆☆");
         break;
-    case 1: $("#result").text("よい");
+    case 1: $("#result").text("☆☆☆☆");
         break;
-    case 2: $("#result").text("普通");
+    case 2: $("#result").text("☆☆☆");
         break;
-    case 3: $("#result").text("まずまず");
+    case 3: $("#result").text("☆☆");
         break;
-    case 4: $("#result").text("よくないかも");
+    case 4: $("#result").text("☆");
         break;
 };
 
@@ -55,6 +47,17 @@ console.log(items[item]);
 
 $("#color").text(colors[color]);
 $("#item").text(items[item]);
-  
 
-});
+};
+
+
+    
+/*
+    const requestData = {
+    star: $("#deta radio[name=horoscope]").val(),
+    blood: $("#deta radio[name=blood]").val(),
+    eto: $("#deta radio[name=eto]").val()
+  };
+*/
+
+
